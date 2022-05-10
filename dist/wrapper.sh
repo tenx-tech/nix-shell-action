6aff2dc2738d0388cc1fe87e33d9b73c62a049d3
Sarfaraz Nawaz <sir_nawaz959@yahoo.com>
2022-05-10 09:55:25 +0530

    Update wrapper.sh

--- dist/wrapper.sh
+++ dist/wrapper.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 echo nixpkgs.
+echo "NAWAZ"
 # nix run nixpkgs. -c  /home/farlion/code/nix/nix-shell-action/lib/script.sh ||
 nix --experimental-features 'nix-command flakes' shell nixpkgs# -c  /home/farlion/code/nix/nix-shell-action/lib/script.sh
       
