672879b14447e3b7ed97daa8d7c1fcb332ebd31c
Sarfaraz Nawaz <sir_nawaz959@yahoo.com>
2022-05-10 09:01:09 +0530

    Comment the problematic line. Maybe that's not even needed

--- dist/wrapper.sh
+++ dist/wrapper.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 echo nixpkgs.
-nix run nixpkgs. -c  /home/farlion/code/nix/nix-shell-action/lib/script.sh ||
+# nix run nixpkgs. -c  /home/farlion/code/nix/nix-shell-action/lib/script.sh ||
 nix --experimental-features 'nix-command flakes' shell nixpkgs# -c  /home/farlion/code/nix/nix-shell-action/lib/script.sh
       
